5f52f27a53a4f5ad4a9eb2c38b97e93f8a52ba2a
gustavokoerich <koerich57@gmail.com>
2024-07-30 22:12:58 -0300

    feat: basic logger

--- src/main/java/com/shorter/shorter/controllers/UrlController.java
+++ src/main/java/com/shorter/shorter/controllers/UrlController.java
@@ -4,6 +4,8 @@ import com.shorter.shorter.dtos.UrlDto;
 import com.shorter.shorter.entities.UrlEntity;
 import com.shorter.shorter.services.UrlService;
 import org.jetbrains.annotations.NotNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.CacheEvict;
@@ -25,21 +27,26 @@ public class UrlController {
     @Autowired
     CacheManager cacheManager;
 
+    private static Logger logger = LoggerFactory.getLogger(UrlController.class);
+
     @Cacheable(key = "#id", value = "url")
     @RequestMapping(path = "/url/{id}", method = RequestMethod.GET)
     public Optional<UrlEntity> findById(@PathVariable UUID id) {
+        logger.info("Find by id {}", id);
         return this.service.findById(id);
     }
 
     @Cacheable(value = "url", key = "#shortUrl")
     @RequestMapping(path = "/short/{shortUrl}", method = RequestMethod.GET)
     public Optional<UrlEntity> findByShortUrl(@PathVariable String shortUrl) {
+        logger.info("Find by shortUrl {}", shortUrl);
         return this.service.findByShortUrl(shortUrl);
     }
 
     @Cacheable(value = "url", key = "#body.defaultUrl")
     @RequestMapping(path = "/default", method = RequestMethod.POST)
     public Optional<UrlEntity> findByDefaultUrl(@RequestBody UrlDto body) {
+        logger.info("Find by defaultUrl {}", body.defaultUrl());
         return this.service.findByDefaultUrl(body.defaultUrl());
     }
 
@@ -51,7 +58,9 @@ public class UrlController {
 
     @RequestMapping(path = "/url", method = RequestMethod.POST)
     public UrlEntity shorterUrl(@RequestBody UrlDto body) {
-        return this.service.shorterUrl(body.defaultUrl());
+        UrlEntity url = this.service.shorterUrl(body.defaultUrl());
+        logger.info("Url shorted from {} to {}", body.defaultUrl(), url.getShortUrl());
+        return url;
     }
 
 
@@ -63,6 +72,8 @@ public class UrlController {
     @RequestMapping(path = "/url/{id}", method = RequestMethod.PUT)
     public UrlEntity updateShortedUrl(@NotNull  @RequestBody UrlDto url,
                                       @PathVariable UUID id) {
-        return this.service.updateShortedUrl(id, url.defaultUrl());
+        UrlEntity updatedUrl = this.service.updateShortedUrl(id, url.defaultUrl());
+        logger.info("Url id {} updated", id);
+        return updatedUrl;
     }
 }
